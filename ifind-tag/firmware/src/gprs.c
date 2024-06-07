fae4aee8ca7f791fffcac8733c5ced7ea2a0f37f
Aleksey Serikov <serikov.ar@phystech.edu>
2024-06-07 19:14:20 +0300

    Собираю проект метки

---
+++ ifind-tag/firmware/src/gprs.c
@@ -0,0 +1,24 @@
+#include "gprs.h"
+
+//char buf[256];
+
+//typedef struct {
+//	char buffer[256];
+//	int data_ptr;
+//	bool started;
+//	bool ready;
+//} mnp_nmea_reader_t;
+//
+//static uint8_t nmea_byte;
+//static mnp_nmea_reader_t nmea_readers[2];
+//static mnp_nmea_reader_t* nmea_active_reader;
+//static uint8_t nmea_active_reader_index = 0;
+
+
+//void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+//{
+//	if (huart->Instance == USART1)
+//	{
+//		
+//	}
+//}
--- ifind-tag/firmware/src/main.c
+++ ifind-tag/firmware/src/main.c
@@ -1,4 +1,4 @@
-#include "stm32f1xx_hal.h"
+//#include "stm32f1xx_hal.h"
 #include "main.h"
 #include "gpio.h"
 #include "usart.h"
@@ -9,6 +9,15 @@ void SystemClock_Config(void);
 extern UART_HandleTypeDef huart1;
 extern UART_HandleTypeDef huart2;
 
+//uint8_t buf[256];
+//
+//void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+//{
+//	if (huart->Instance == USART1)
+//	{
+//		HAL_UART_Receive_IT(&huart1, (uint8_t*)buf, 1);
+//	}
+//}
 
 int main()
 {
@@ -17,7 +26,11 @@ int main()
 	MX_GPIO_Init();
 	MX_USART1_UART_Init();
 	MX_USART2_UART_Init();
-	
+//	HAL_UART_Receive_IT(&huart1, (uint8_t*)buf, 1);
+//	
+//	uint8_t test[2] = {'A', 'T' };
+//	
+//	HAL_UART_Transmit(&huart1, test, 2, 100);
 	while (1)
 	{
 	         
